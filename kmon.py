d3d835907a52a6b82530bf3ff0931fa3f87c33c8
Andrey Krushelnitskiy <andrey.krushelnitsky@myoffice.team>
2020-09-14 09:16:32 +0300

    add correct status code

--- kmon.py
+++ kmon.py
@@ -1,26 +1,37 @@
 #!/usr/bin/python3
 import requests, yaml
+
 from datetime import datetime, date, time
+import ssl
+import OpenSSL
+
+debug = False
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:54.0) Gecko/20100101 Firefox/54.0","Connection":"close","Accept-Language":"en-US,en;q=0.5","Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Upgrade-Insecure-Requests":"1"}
+timeout = 15
+
+if not debug:
+    from requests.packages.urllib3.exceptions import InsecureRequestWarning
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 file = open(r'sites.yaml')
 sites = yaml.load(file, Loader=yaml.FullLoader)
 
+def get_SSL_Expiry_Date(host, port):
+    cert = ssl.get_server_certificate((host, 443))
+    x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
+    print(datetime.strptime(x509.get_notAfter().decode('ascii'), '%Y%m%d%H%M%SZ'))
+
 def layout(res, check):
-    print(datetime.now(), site['name'], status_code, float(res.elapsed.total_seconds()), check)
+    print(datetime.now(), site['http'], res.status_code, float(res.elapsed.total_seconds()), check)
 
 def handler(self, parameter_list):
     pass
 
 for site in sites:
-    status_code = None
-    try:
-        res = requests.get(site['http'])
-        status_code = res.status_code
-    except:
-        pass
+    res = requests.get(site['http'], headers=headers, timeout=timeout, verify=False, allow_redirects=False)
     # http code check
     if 'status_code' in site.keys():
-        if str(status_code) == str(site['status_code']):
+        if str(res.status_code) == str(site['status_code']):
             check = "status_code:OK"
         else:
             check = "status_code:ERROR"
@@ -33,7 +44,8 @@ for site in sites:
             check = "load_time:ERROR"
         layout(res, check)
     # checl ssl expiration
-    # TO DO
+    if 'min_ssl_expiry_days' in site.keys():
+        get_SSL_Expiry_Date(site['host'], 443)
 
     # check work in http res
     # TO DO
