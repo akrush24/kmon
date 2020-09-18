a88c8b42d2454cae954ea140447291a9c4f89463
Andrey Krushelnitskiy <andrey.krushelnitsky@myoffice.team>
2020-09-18 23:26:47 +0300

    add ssl check, work

--- kmon.py
+++ kmon.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 import requests, yaml
 
-from datetime import datetime, date, time
+from datetime import datetime, date, time, timedelta
 import ssl
 import OpenSSL
+import pytz
+import socket
+import re
+from urllib.parse import urlparse
+import urllib.request, json
+import email.message
+import smtplib
+from urllib.request import Request, urlopen, ssl, socket
+from urllib.error import URLError, HTTPError
 
 debug = False
 headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:54.0) Gecko/20100101 Firefox/54.0","Connection":"close","Accept-Language":"en-US,en;q=0.5","Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Upgrade-Insecure-Requests":"1"}
@@ -16,36 +25,67 @@ if not debug:
 file = open(r'sites.yaml')
 sites = yaml.load(file, Loader=yaml.FullLoader)
 
-def get_SSL_Expiry_Date(host, port):
-    cert = ssl.get_server_certificate((host, 443))
-    x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
-    print(datetime.strptime(x509.get_notAfter().decode('ascii'), '%Y%m%d%H%M%SZ'))
-
 def layout(res, check):
     print(datetime.now(), site['http'], res.status_code, float(res.elapsed.total_seconds()), check)
 
-def handler(self, parameter_list):
-    pass
+def action(action):
+    print("AXTUNG")
+
+now = datetime.now(pytz.utc)
 
 for site in sites:
     res = requests.get(site['http'], headers=headers, timeout=timeout, verify=False, allow_redirects=False)
-    # http code check
+
+    # http code check #
     if 'status_code' in site.keys():
         if str(res.status_code) == str(site['status_code']):
-            check = "status_code:OK"
+            check = "Status_code:OK"
         else:
-            check = "status_code:ERROR"
+            check = "Status_code:ERROR"
+            if 'search' in site.keys():
+                action(site['search'])
         layout(res, check)
-    # load time check
+
+    # load time check #
     if 'load_time' in site.keys():
         if float(res.elapsed.total_seconds()) < float(site['load_time']):
-            check = "load_time:OK"
+            check = "Load_time:OK"
         else:
-            check = "load_time:ERROR"
+            check = "Load_time:ERROR"
+            if 'search' in site.keys():
+                action(site['search'])
         layout(res, check)
-    # checl ssl expiration
+
+    # check context #
+    if 'search' in site.keys():
+        if re.search(site['search'], res.text):
+            check = "Search:OK"
+        else:
+            check = "Search:ERROR"
+            if 'search' in site.keys():
+                action(site['search'])
+        layout(res, check)
+
+    # checl ssl expiration @
     if 'min_ssl_expiry_days' in site.keys():
-        get_SSL_Expiry_Date(site['host'], 443)
+        port = '443'
+        context = ssl.create_default_context()
+        with socket.create_connection((site['host'], port)) as sock:
+            with context.wrap_socket(sock, server_hostname=site['host']) as ssock:
+                notAfter = datetime.strptime(ssock.getpeercert()['notAfter'], r"%b %d %H:%M:%S %Y %Z").replace(tzinfo=pytz.UTC)
+                subject = ssock.getpeercert()['subject'][0][0][1]
+        
+        ssl_check_date = datetime.now() + timedelta(days=site['min_ssl_expiry_days'])
+        if ssl_check_date.replace(tzinfo=pytz.UTC) > notAfter.replace(tzinfo=pytz.UTC):
+            check = "SSL EXPIRE ERROR"
+        else:
+            check = "SSL EXPIRE:OK"
+        layout(res, check)
+        if subject != site['host']:
+            check = "SSL SUBJECT:ERROR"
+        else:
+            check = "SSL SUBJECT:OK"
+        layout(res, check)
 
     # check work in http res
     # TO DO
