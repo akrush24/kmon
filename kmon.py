102d11e1033b4305a63f18834dac07ee2d1a6751
Andrey Krushelnitskiy <andrey.krushelnitsky@myoffice.team>
2020-09-21 23:08:56 +0300

    mon day

--- kmon.py
+++ kmon.py
@@ -17,11 +17,8 @@ if not debug:
     from requests.packages.urllib3.exceptions import InsecureRequestWarning
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-file = open(r'{}/config.yaml'.format(os.path.abspath(os.path.dirname(sys.argv[0]))))
-config = yaml.load(file, Loader=yaml.FullLoader)
-
-def layout(res, check):
-    print("[{}] {} {}".format(datetime.now(), site['name'], check))
+def layout(name, check):
+    print("[{}] {} {}".format(datetime.now(), name, check))
 
 def action(heandler, action):
     if heandler == 'telegram':
@@ -32,71 +29,87 @@ def check_ping(hostname):
     # and then check the response...
     return response
 
-# = # = # = # = # = # = # = # = # 
-for site in config['checks']:
-    if 'url' in site.keys():
-        res = requests.get(site['url'], headers=headers, timeout=timeout, verify=False, allow_redirects=False)
+def main():
+    file = open(r'{}/config.yaml'.format(os.path.abspath(os.path.dirname(sys.argv[0]))))
+    config = yaml.load(file, Loader=yaml.FullLoader)
 
-    # http code check #
-    if 'status_code' in site.keys():
-        if str(res.status_code) == str(site['status_code']):
-            check = "Status_code:OK [{}]".format(res.status_code)
-        else:
-            check = "Status_code:ERROR [{}]".format(res.status_code)
-            action('telegram', site['url']+", "+check)
-        if res.status_code == 302:
-            print("{} Redireced to: {}".format(site['url'], res.headers['Location']))
-        layout(res, check)
+    for site in config['checks']:
+        
+        if 'url' in site.keys():
+            res = requests.get(site['url'], headers=headers, timeout=timeout, verify=False, allow_redirects=False)
 
-    # load time check #
-    if 'load_time' in site.keys():
-        if float(res.elapsed.total_seconds()) < float(site['load_time']):
-            check = "Load_time:OK [{}]".format(str(res.elapsed.total_seconds()))
-        else:
-            check = "Load_time:ERROR [{}]".format(str(res.elapsed.total_seconds()))
-            action('telegram', site['host']+", "+check)
-        layout(res, check)
+        # http code check #
+        if 'status_code' in site.keys():
+            if str(res.status_code) == str(site['status_code']):
+                check = "Status_code:OK [{}]".format(res.status_code)
+            else:
+                check = "Status_code:ERROR [{}]".format(res.status_code)
+                action('telegram', site['url']+", "+check)
+            if res.status_code == 302:
+                print("{} Redireced to: {}".format(site['url'], res.headers['Location']))
+            layout(site['name'], check)
 
-    # check context #
-    if 'search' in site.keys():
-        if re.search(site['search'], res.text):
-            check = "Search:OK"
-        else:
-            check = "Search:ERROR"
-            action('telegram', site['url']+", "+check)
-        layout(res, check)
-
-    # checl ssl expiration #
-    if 'min_ssl_expiry_days' in site.keys():
-        port = '443'
-        context = ssl.create_default_context()
-        try:
-            with socket.create_connection((site['host'], port)) as sock:
-                with context.wrap_socket(sock, server_hostname=site['host']) as ssock:
-                    notAfter = datetime.strptime(ssock.getpeercert()['notAfter'], r"%b %d %H:%M:%S %Y %Z").replace(tzinfo=pytz.UTC)
-                    subject = ssock.getpeercert()['subject'][0][0][1]
-            ssl_check_date = datetime.now() + timedelta(days=site['min_ssl_expiry_days'])
-            if ssl_check_date.replace(tzinfo=pytz.UTC) > notAfter.replace(tzinfo=pytz.UTC):
-                check = "SSL EXPIRE ERROR ["+notAfter+"]"
+        # load time check #
+        if 'load_time' in site.keys():
+            if float(res.elapsed.total_seconds()) < float(site['load_time']):
+                check = "Load_time:OK [{}]".format(str(res.elapsed.total_seconds()))
+            else:
+                check = "Load_time:ERROR [{}]".format(str(res.elapsed.total_seconds()))
                 action('telegram', site['host']+", "+check)
-            else:
-                check = "SSL EXPIRE:OK [{}]".format(notAfter)
-            layout(res, check)
-            if subject != site['host']:
-                check = "SSL SUBJECT:ERROR"
-                action('telegram', site['host']+", "+check)
-            else:
-                check = "SSL SUBJECT:OK [{}]".format(notAfter)
-        except Exception as e:
-            check = "SSL CHECK ERROR: {}".format(e)
+            layout(site['name'], check)
 
-    # ICMP (ping) server check #
-    if 'icmp' in site.keys():
-        if site['icmp']:
-            ping_status = check_ping(site['host'])
-            if ping_status == 0:
-                check = "ICMP:OK"
+        # check context #
+        if 'search' in site.keys():
+            if re.search(site['search'], res.text):
+                check = "Search:OK"
             else:
-                check = "ICMP:ERROR [{}]".format(ping_status)
-                action('telegram', "{}, {}".format(site['host'],check))
-            layout(res, check)
+                check = "Search:ERROR"
+                action('telegram', site['url']+", "+check)
+            layout(site['name'], check)
+
+        # checl ssl expiration #
+        if 'min_ssl_expiry_days' in site.keys():
+            port = '443'
+            context = ssl.create_default_context()
+            if (not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None)):
+                ssl._create_default_https_context = ssl._create_unverified_context
+            try:
+                with socket.create_connection((site['host'], port)) as sock:
+                    with context.wrap_socket(sock, server_hostname=site['host']) as ssock:
+                        notAfter = datetime.strptime(ssock.getpeercert()['notAfter'], r"%b %d %H:%M:%S %Y %Z").replace(tzinfo=pytz.UTC)
+                        subject = ssock.getpeercert()['subject'][0][0][1]
+                
+                ssl_check_date = datetime.now() + timedelta(days=site['min_ssl_expiry_days'])
+                if ssl_check_date.replace(tzinfo=pytz.UTC) > notAfter.replace(tzinfo=pytz.UTC):
+                    check = "SSL EXPIRE ERROR ["+notAfter+"]"
+                    action('telegram', site['host']+", "+check)
+                else:
+                    check = "SSL EXPIRE:OK [{}]".format(notAfter)
+                    layout(site['name'], check)
+                
+                if subject != site['host']:
+                    check = "SSL SUBJECT:ERROR"
+                    action('telegram', site['host']+", "+check)
+                else:
+                    check = "SSL SUBJECT:OK [{}]".format(subject)
+                    layout(site['name'], check)
+                
+            except Exception as e:
+                check = "SSL CHECK ERROR: {}".format(e)
+                layout(site['name'], check)
+
+        # ICMP (ping) server check #
+        if 'icmp' in site.keys():
+            if site['icmp']:
+                ping_status = check_ping(site['host'])
+                if ping_status == 0:
+                    check = "ICMP:OK"
+                else:
+                    check = "ICMP:ERROR [{}]".format(ping_status)
+                    action('telegram', "{}, {}".format(site['host'],check))
+                layout(site['name'], check)
+
+
+
+if __name__ == "__main__":
+    main()
