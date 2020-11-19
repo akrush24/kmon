8d419513d9084722b53882e7da298732af3eed4f
Andrey Krushelnitskiy <andrey.krushelnitsky@myoffice.team>
2020-11-19 22:49:35 +0300

    add click support

--- kmon.py
+++ kmon.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import requests
 import yaml
-
 import pytz
 import socket
 import re
@@ -9,118 +8,153 @@ import ssl
 import sys
 import os
 from datetime import datetime, timedelta
+import click
+import logging
+
+# logger
+logger = logging.getLogger('kmon')
+logger.setLevel(logging.DEBUG)
+logging_handler = logging.StreamHandler()
+logging_handler.setFormatter(logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s'))
+logger.addHandler(logging_handler)
 
 
 debug = False
 headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:54.0) Gecko/20100101 Firefox/54.0","Connection":"close","Accept-Language":"en-US,en;q=0.5","Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Upgrade-Insecure-Requests":"1"}
 timeout = 15
-file = open(r'{}/config.yaml'.format(os.path.abspath(os.path.dirname(sys.argv[0]))))
-config = yaml.load(file, Loader=yaml.FullLoader)
+
 if not debug:
     from requests.packages.urllib3.exceptions import InsecureRequestWarning
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-def layout(name, check):
-    print("[{}] {} {}".format(datetime.now(), name, check))
 
 def heandler(name, action, revert, success):
     if (success == False and revert == False) or \
         (success == True and revert == True):
         if name == 'telegram':
-            requests.get('https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'.format(config['telegram']['ttoken'], config['telegram']['tuserid'], action))
+            requests.get(
+                            'https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'.format(
+                                config['telegram']['ttoken'], config['telegram']['tuserid'], action
+                            )
+                        )
+
 
 def check_ping(hostname):
     response = os.system("ping -c 1 -w 2 {} 2>/dev/null 1>&2".format(hostname))
     # and then check the response...
     return response
 
-def main():
 
-    for check in config['checks']:
+@click.group()
+@click.option('--config', '-c', envvar='CONFIGFILENAME', required=False, 
+                help="Config File Name", default="config.yaml")
+@click.pass_context
+def cli(ctx, config):
+    ctx.ensure_object(dict)
+    file = open(r'{}/{}'.format( os.path.abspath(os.path.dirname(sys.argv[0])), config ))
+    ctx.obj['config'] = yaml.load(file, Loader=yaml.FullLoader)
 
-        # revert меняет значение проверки, если проверка  
-        # завершилась неудачей это считается ОК или наоборот
-        try:
-            check['revert']
-        except:
-            check['revert'] = False
 
-        if 'url' in check.keys():
-            res = requests.get(check['url'], headers=headers, timeout=timeout, verify=False, allow_redirects=False)
+@click.option('--name', '-n', required=False, multiple=True, help="check name")
+@cli.command()
+@click.pass_context
+def run(ctx, name):
 
-        # http code check #
-        if 'status_code' in check.keys():
-            if str(res.status_code) == str(check['status_code']):
-                message = "Status_code:OK [{}]".format(res.status_code)
-            else:
-                message = "Status_code:ERROR [{}]".format(res.status_code)
-                heandler('telegram', check['url']+", "+check)
-            if res.status_code == 302:
-                print("{} Redireced to: {}".format(check['url'], res.headers['Location']))
-            layout(check['name'], message)
+    # проходимся по всем проверкам
+    for check in ctx.obj['config']['checks']:
 
-        # load time check #
-        if 'load_time' in check.keys():
-            if float(res.elapsed.total_seconds()) < float(check['load_time']):
-                message = "Load_time:OK [{}]".format(str(res.elapsed.total_seconds()))
-            else:
-                message = "Load_time:ERROR [{}]".format(str(res.elapsed.total_seconds()))
-                heandler('telegram', check['host']+", "+check)
-            layout(check['name'], message)
+        # если имя проверки указаны через агрументы то работаем только с ними
+        if (len(name) > 0 and check['name'] in name) or len(name) == 0:
 
-        # check context #
-        if 'search' in check.keys():
-            if re.search(check['search'], res.text):
-                message = "Search:OK"
-                heandler('telegram', check['name']+", "+message, revert=check['revert'], success=True)
-            else:
-                message = "Search:ERROR"
-                heandler('telegram', check['name']+", "+message, revert=check['revert'], success=False)
-            layout(check['name'], message)
-
-        # checl ssl expiration #
-        if 'min_ssl_expiry_days' in check.keys():
-            port = '443'
-            context = ssl.create_default_context()
-            if (not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None)):
-                ssl._create_default_https_context = ssl._create_unverified_context
+            # revert меняет значение проверки, если проверка  
+            # завершилась неудачей это считается ОК или наоборот
             try:
-                with socket.create_connection((check['host'], port)) as sock:
-                    with context.wrap_socket(sock, server_hostname=check['host']) as ssock:
-                        notAfter = datetime.strptime(ssock.getpeercert()['notAfter'], r"%b %d %H:%M:%S %Y %Z").replace(tzinfo=pytz.UTC)
-                        subject = ssock.getpeercert()['subject'][0][0][1]
-                
-                ssl_check_date = datetime.now() + timedelta(days=check['min_ssl_expiry_days'])
-                if ssl_check_date.replace(tzinfo=pytz.UTC) > notAfter.replace(tzinfo=pytz.UTC):
-                    message = "SSL EXPIRE ERROR ["+notAfter+"]"
-                    heandler('telegram', check['host']+", "+message)
-                else:
-                    message = "SSL EXPIRE:OK [{}]".format(notAfter)
-                    layout(check['name'], message)
-                
-                if subject != check['host']:
-                    message = "SSL SUBJECT:ERROR"
-                    heandler('telegram', check['host']+", "+message)
-                else:
-                    message = "SSL SUBJECT:OK [{}]".format(subject)
-                    layout(check['name'], message)
-                
-            except Exception as e:
-                message = "SSL CHECK ERROR: {}".format(e)
-                layout(check['name'], message)
+                check['revert']
+            except:
+                check['revert'] = False
 
-        # ICMP (ping) server check #
-        if 'icmp' in check.keys():
-            if check['icmp']:
-                ping_status = check_ping(check['host'])
-                if ping_status == 0:
-                    message = "ICMP:OK"
+            if 'url' in check.keys():
+                res = requests.get(check['url'], headers=headers, timeout=timeout, verify=False, allow_redirects=False)
+
+            # http code check #
+            if 'status_code' in check.keys():
+                if str(res.status_code) == str(check['status_code']):
+                    message = "Status_code: [{}]".format(res.status_code)
+                    logger.info('{} {}'.format(check['name'], message))
                 else:
-                    message = "ICMP:ERROR [{}]".format(ping_status)
-                    heandler('telegram', "{}, {}".format(check['host'], message))
-                layout(check['name'], message)
+                    message = "Status_code: [{}]".format(res.status_code)
+                    heandler('telegram', check['url']+", "+check, revert=check['revert'], success=True)
+                    logger.error('{} {}'.format(check['name'], message))
+                if res.status_code == 302:
+                    print("{} Redireced to: {}".format(check['url'], res.headers['Location']))
+                
+
+            # load time check #
+            if 'load_time' in check.keys():
+                if float(res.elapsed.total_seconds()) < float(check['load_time']):
+                    message = "Load_time: [{}]".format(str(res.elapsed.total_seconds()))
+                    logger.info('{} {}'.format(check['name'], message))
+                else:
+                    message = "Load_time: [{}]".format(str(res.elapsed.total_seconds()))
+                    heandler('telegram', check['host']+", "+check, revert=check['revert'], success=True)
+                    logger.error('{} {}'.format(check['name'], message))
+
+            # check context #
+            if 'search' in check.keys():
+                if re.search(check['search'], res.text):
+                    message = "Search:"
+                    heandler('telegram', check['name']+", "+message, revert=check['revert'], success=True)
+                    logger.info('{} {}'.format(check['name'], message))
+                else:
+                    message = "Search:"
+                    heandler('telegram', check['name']+", "+message, revert=check['revert'], success=False)
+                    logger.error('{} {}'.format(check['name'], message))
+
+            # checl ssl expiration #
+            if 'min_ssl_expiry_days' in check.keys():
+                port = '443'
+                context = ssl.create_default_context()
+                if (not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None)):
+                    ssl._create_default_https_context = ssl._create_unverified_context
+                try:
+                    with socket.create_connection((check['host'], port)) as sock:
+                        with context.wrap_socket(sock, server_hostname=check['host']) as ssock:
+                            notAfter = datetime.strptime(ssock.getpeercert()['notAfter'], r"%b %d %H:%M:%S %Y %Z").replace(tzinfo=pytz.UTC)
+                            subject = ssock.getpeercert()['subject'][0][0][1]
+                    
+                    ssl_check_date = datetime.now() + timedelta(days=check['min_ssl_expiry_days'])
+                    if ssl_check_date.replace(tzinfo=pytz.UTC) > notAfter.replace(tzinfo=pytz.UTC):
+                        message = "SSL EXPIRE  ["+notAfter+"]"
+                        heandler('telegram', check['host']+", "+message, revert=check['revert'], success=True)
+                    else:
+                        message = "SSL EXPIRE: [{}]".format(notAfter)
+                        logger.info('{} {}'.format(check['name'], message))
+                    
+                    if subject != check['host']:
+                        message = "SSL SUBJECT:"
+                        heandler('telegram', check['host']+", "+message, revert=check['revert'], success=True)
+                        logger.error('{} {}'.format(check['name'], message))
+                    else:
+                        message = "SSL SUBJECT: [{}]".format(subject)
+                        logger.info('{} {}'.format(check['name'], message))
+                    
+                except Exception as e:
+                    message = "SSL CHECK: {}".format(e)
+                    logger.error('{} {}'.format(check['name'], message))
+
+            # ICMP (ping) server check #
+            if 'icmp' in check.keys():
+                if check['icmp']:
+                    ping_status = check_ping(check['host'])
+                    if ping_status == 0:
+                        message = "ICMP:"
+                        logger.info('{} {}'.format(check['name'], message))
+                    else:
+                        message = "ICMP: [{}]".format(ping_status)
+                        heandler('telegram', "{}, {}".format(check['host'], message), revert=check['revert'], success=True)
+                        logger.error('{} {}'.format(check['name'], message))
 
 
 
 if __name__ == "__main__":
-    main()
+    cli()
